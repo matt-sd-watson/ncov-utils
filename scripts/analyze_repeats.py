4248bdcb8019f4792e64ead05cf6cca67fe8d41f
Matthew Watson <matthew.watson@oahpp.ca>
2021-04-12 17:26:18 -0400

    Refactor python scripts for command line use

--- scripts/analyze_repeats.py
+++ scripts/analyze_repeats.py
@@ -1,123 +1,110 @@
 import pandas as pd
 from Bio import SeqIO
 import argparse
-import os
 import numpy as np
 
-"""
-This script processes inputs for ivar repeats to assess their similarity and coverage statistics
-Inputs required: 
-	- Multi-fasta for the sampes to analyze
-	- CSV output of snp-dists run with molten format and ambiguous bases excluded
-	- SNPs only FASTA generated through snp-dists with ambiguous bases enabled
-Outputs: 
-	- Various CSV files with filenames depending on their similarity, with ambiguous bases (N) counts and mixed
-	SNP positions
-"""
 
-parser = argparse.ArgumentParser(description='Analyze a set of sample repeats for similarity and coverage statistics')
-parser.add_argument('--multi_fasta', '-f', type=str, help='Multi-fasta to evaluate sequences from', required=True)
-parser.add_argument('--snp_dists', '-s', type=str, help='CSV file of SNP differences from snp-dists in molten format',
-                    required=True)
-parser.add_argument('--output_dir', '-o', type=str, help='output directory for single modified fasta files',
-                    required=True)
-parser.add_argument('--fasta_mixed', '-m', type=str, help='Multi-fasta without exclusively ACGT to count mixed positions',
-                    required=True)
-
-args = parser.parse_args()
-
-snp_dists = pd.read_csv(args.snp_dists, header=None)
-snp_dists.columns = ['sam_1', 'comparing', 'distance']
-
-# filter if the sample 1 name is in the second column, and filter by snp distances
-new_frame = snp_dists[[x[0] in x[1] for x in zip(snp_dists['sam_1'], snp_dists['comparing'])]]
-identical_frame = new_frame.loc[(new_frame['sam_1'] != new_frame['comparing'])][snp_dists.distance == 0]
-non_identical_frame = new_frame.loc[(new_frame['sam_1'] != new_frame['comparing'])][snp_dists.distance != 0]
-
-# if the names are not similar or identical but the same distances are identical, write separately
-diff_name_identical = snp_dists[~snp_dists.sam_1.isin(identical_frame.sam_1)]
-diff_name_identical = diff_name_identical[~diff_name_identical.sam_1.isin(identical_frame.comparing)]
-diff_name_identical = diff_name_identical[snp_dists.apply(lambda x: x.sam_1 not in x.comparing, axis=1)][snp_dists.distance == 0]
-
-if len(diff_name_identical) != 0:
-    diff_name_identical.to_csv("different_names_identical.csv", index=False)
-
-
-fasta_sequences = SeqIO.parse(open(args.multi_fasta), 'fasta')
-
-# get a count of the ambiguous (N) positions for identical sequences, or add the id to the not identical category
-sam_n_counts = []
-non_identical = []
-for record in fasta_sequences:
-    if record.id in identical_frame.sam_1.unique() or record.id in identical_frame.comparing.unique():
-        keys = ['sample_name', 'N_counts']
-        values = [record.id, record.seq.count("N")]
-        to_add = dict(zip(keys, values))
-        sam_n_counts.append(to_add)
-    else:
-        non_identical.append(record.id)
-
-n_counts_frame = pd.DataFrame(sam_n_counts)
-
-if n_counts_frame.shape[0] != 0:
-    n_counts_frame.sort_values(by=['sample_name'], ascending=True).to_csv("identical_sequences.csv", index=False)
-
-if len(non_identical) != 0:
-	with open("Non-identical sequences.txt", "w") as handle:
-		for lines in non_identical:
-			handle.write(lines+"\n")
-
-print("Identical Sequences:")
-if n_counts_frame.shape[0] != 0:
-    print(n_counts_frame.sort_values(by=['sample_name'], ascending=True))
-else:
-    print("None")
-
-print("Non-Identical sequences:")
-if len(non_identical) != 0:
-    for elem in non_identical:
-        print(elem)
-else:
-    print("None")
-
-# group the frame by similar name, and get the sample with the lowest n count per group
 def get_grouping(data_frame):
     data_frame['standard_name'] = data_frame['sample_name'].map(lambda x: x.rstrip('-v2|-v3|-v4|-v5|-v6'))
     min_frame = data_frame.loc[data_frame.groupby('standard_name').N_counts.idxmin()][["sample_name", "N_counts"]]
     return data_frame, min_frame
 
-# get the number and identity of mixed (non ATCGN) positions from the mixed SNPs FASTA
-fasta_mixed = SeqIO.parse(open(args.fasta_mixed), 'fasta')
-nuc = ['A', 'T', 'C', 'G', 'N']
-mixed_post = []
-pos = {}
-for record in fasta_mixed:
-    mixed_pos = []
-    if record.id not in non_identical and record.id != "MN908947":
-        count = 0
-        for index, item in enumerate(record.seq):
-            if item not in nuc:
-                count += 1
-                mixed_pos.append("{}:{}".format(item, int(index + 1)))
 
-        keys = ['sample_name', 'mixed_counts']
-        values = [str(record.id), count]
-        to_add = dict(zip(keys, values))
-        mixed_post.append(to_add)
-        pos[str(record.id)] = ';'.join(n.strip("[]") for n in mixed_pos)
+def main():
+    parser = argparse.ArgumentParser(description='Analyze a set of sample repeats for similarity and coverage statistics')
+    parser.add_argument('--multi_fasta', '-f', type=str, help='Multi-fasta to evaluate sequences from', required=True)
+    parser.add_argument('--snp_dists', '-s', type=str, help='CSV file of SNP differenes from snp-dists in molten format',
+                    required=True)
+    parser.add_argument('--output_dir', '-o', type=str, help='output directory for single modified fasta files',
+                    required=True)
+    parser.add_argument('--fasta_mixed', '-m', type=str, help='Multi-fasta without exclusively ACGT to count mixed positions',
+                    required=True)
+
+    args = parser.parse_args()
+
+    snp_dists = pd.read_csv(args.snp_dists, header=None)
+    snp_dists.columns = ['sam_1', 'comparing', 'distance']
+
+    # filter if the sample 1 name is in the second column, and filter by snp distances
+    new_frame = snp_dists[[x[0] in x[1] for x in zip(snp_dists['sam_1'], snp_dists['comparing'])]]
+    identical_frame = new_frame.loc[(new_frame['sam_1'] != new_frame['comparing'])][snp_dists.distance == 0]
+    non_identical_frame = new_frame.loc[(new_frame['sam_1'] != new_frame['comparing'])][snp_dists.distance != 0]
+
+    diff_name_identical = snp_dists[~snp_dists.sam_1.isin(identical_frame.sam_1)]
+    diff_name_identical = diff_name_identical[~diff_name_identical.sam_1.isin(identical_frame.comparing)]
+    diff_name_identical = diff_name_identical[snp_dists.apply(lambda x: x.sam_1 not in x.comparing, axis=1)][snp_dists.distance == 0]
+
+    if len(diff_name_identical) != 0:
+        diff_name_identical.to_csv("different_names_identical.csv", index=False)
+
+    fasta_sequences = SeqIO.parse(open(args.multi_fasta), 'fasta')
+
+    sam_n_counts = []
+    non_identical = []
+    for record in fasta_sequences:
+        if record.id in identical_frame.sam_1.unique() or record.id in identical_frame.comparing.unique():
+            keys = ['sample_name', 'N_counts']
+            values = [record.id, record.seq.count("N")]
+            to_add = dict(zip(keys, values))
+            sam_n_counts.append(to_add)
+        else:
+            non_identical.append(record.id)
+
+    n_counts_frame = pd.DataFrame(sam_n_counts)
+
+    if n_counts_frame.shape[0] != 0:
+        n_counts_frame.sort_values(by=['sample_name'], ascending=True).to_csv("identical_sequences.csv", index=False)
+
+    if len(non_identical) != 0:
+        with open("Non-identical sequences.txt", "w") as handle:
+            for lines in non_identical:
+                handle.write(lines+"\n")
+
+    print("Identical Sequences:")
+    if n_counts_frame.shape[0] != 0:
+        print(n_counts_frame.sort_values(by=['sample_name'], ascending=True))
+    else:
+        print("None")
+
+    print("Non-Identical sequences:")
+    if len(non_identical) != 0:
+        for elem in non_identical:
+            print(elem)
+    else:
+        print("None")
+
+    fasta_mixed = SeqIO.parse(open(args.fasta_mixed), 'fasta')
+    nuc = ['A', 'T', 'C', 'G', 'N']
+    mixed_post = []
+    pos = {}
+    for record in fasta_mixed:
+        mixed_pos = []
+        if record.id not in non_identical and record.id != "MN908947":
+            count = 0
+            for index, item in enumerate(record.seq):
+                if item not in nuc:
+                    count += 1
+                    mixed_pos.append("{}:{}".format(item, int(index + 1)))
+
+            keys = ['sample_name', 'mixed_counts']
+            values = [str(record.id), count]
+            to_add = dict(zip(keys, values))
+            mixed_post.append(to_add)
+            pos[str(record.id)] = ';'.join(n.strip("[]") for n in mixed_pos)
+
+    mixed_counts = pd.DataFrame(mixed_post)
+    positions_frame = pd.DataFrame(pos.items(), columns=['sample_name', 'mixed_positions'])
+
+    if n_counts_frame.shape[0] != 0:
+        stripped_frame, grouped_min_frame = get_grouping(n_counts_frame)
+        final_frame = mixed_counts.merge(positions_frame, on='sample_name', how='left'). \
+            merge(stripped_frame, on='sample_name', how='left').drop(['standard_name'], axis=1)
+        final_frame['exclude_analysis'] = np.where(final_frame['sample_name'].isin(grouped_min_frame['sample_name']),
+                                                   "N", "Y")
+        if mixed_counts.shape[0] != 0:
+            final_frame.sort_values(by=['sample_name']).to_csv("identical_sequences.csv", index=False)
 
 
-mixed_counts = pd.DataFrame(mixed_post)
-positions_frame = pd.DataFrame(pos.items(), columns=['sample_name', 'mixed_positions'])
+if __name__ == '__main__':
+    main()
 
-# merge mixed counts and positions together, and create exclude analysis category
-# if the sample has the lowest N count in the naming group, do not exclude from analysis
-if n_counts_frame.shape[0] != 0:
-    stripped_frame, grouped_min_frame = get_grouping(n_counts_frame)
-    final_frame = mixed_counts.merge(positions_frame, on='sample_name', how='left'). \
-        merge(stripped_frame, on='sample_name', how='left').drop(['standard_name'], axis=1)
-    final_frame['exclude_analysis'] = np.where(final_frame['sample_name'].isin(grouped_min_frame['sample_name']), "N", "Y")
-    if mixed_counts.shape[0] != 0:
-        final_frame.sort_values(by=['sample_name']).to_csv("identical_sequences.csv", index=False)
-
-	
--- scripts/identify_repeat_samples.py
+++ scripts/identify_repeat_samples.py
@@ -6,41 +6,48 @@ import argparse
 This script takes an input a directory containing output ncov-19 samples from the ivar pipeline
 For PHO samples, the way to designate a repeat sample is to place -v# after the unique WGS_Id, where
 # is the repeat number
-The script is designed to find all previous versions of the sample 
-i.e. if sample-v3 is in the directory, the output will save sample, sample-v2, and sample-v3
+The script is esigned to find all previous versions of the sample 
+i.e. if sample-v3 is in the directory, the output will save sample, and sample-v2
 
 output: 
 	= txt file containing all the sample repeat names as well as the original and previous sample names. 
 	The output txt file is suitable for use with faSomeRecords to subset the master multi-fasta
 """
 
-parser = argparse.ArgumentParser(description='Identify the set of sample repeats from an input directory')
-parser.add_argument('--input_dir', '-i', type=str, help='Directory containing ncov samples', required=True)
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Identify the set of sample repeats from an input directory')
+    parser.add_argument('--input_dir', '-i', type=str, help='Directory containing ncov samples', required=True)
 
-files = [f for f in listdir(args.input_dir) if isfile(join(args.input_dir, f))]
+    args = parser.parse_args()
+
+    files = [f for f in listdir(args.input_dir) if isfile(join(args.input_dir, f))]
 
 # get all unique names of samples that are repeats, denoted by -v#
-repeat_names = set([i.split('_S')[0] for i in files if '-v' in i])
+    repeat_names = set([i.split('_S')[0] for i in files if '-v' in i])
 
 # create a dict of the repeat name and its repeat number (i.e. -v3 = 3)
-strip_number = [i.split('-v')[1] for i in repeat_names]
-strip_dict = dict(zip(repeat_names, strip_number))
+    strip_number = [i.split('-v')[1] for i in repeat_names]
+    strip_dict = dict(zip(repeat_names, strip_number))
 
-total_names = []
+    total_names = []
 
-for key, value in strip_dict.items():
-    # append the original sample name
-    total_names.append(key.split('-v')[0])
-    if int(value) <= 2:
-    # if the repeat is -v2, append it
-        total_names.append(str(key.split('-v')[0] + "-v" + str(2)))
-    else:
-    # is the repeat is -v3 or higher, append all previous repeat names in addition to latest/highest repeat
-        for i in range(2, int(value) + 1, 1):
-            total_names.append(str(key.split('-v')[0] + "-v" + str(i)))
+    for key, value in strip_dict.items():
+        # append the original sample name
+        total_names.append(key.split('-v')[0])
+        if int(value) <= 2:
+            # if the repeat is -v2, append it
+            total_names.append(str(key.split('-v')[0] + "-v" + str(2)))
+        else:
+            # is the repeat is -v3 or higher, append all previous repeat names in addition to latest/highest repeat
+            for i in range(2, int(value) + 1, 1):
+                total_names.append(str(key.split('-v')[0] + "-v" + str(i)))
 
-with open("repeats.txt", "w") as handle:
-    for line in sorted(set(total_names)):
-        handle.write(line + "\n")
+    with open("repeats.txt", "w") as handle:
+        for line in sorted(set(total_names)):
+            handle.write(line + "\n")
+
+
+if __name__ == "__main__":
+    main()
+
